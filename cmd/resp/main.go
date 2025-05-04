31963ed0d8d56893fda1042919b3985054b64523
Loknath Dhar <loknath-dhar@pm.me>
2025-05-04 15:48:07 +0600

    deleted comments

--- cmd/resp/main.go
+++ cmd/resp/main.go
@@ -9,23 +9,6 @@ import (
 )
 
 func main() {
-
-	// err := config.Config()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// configure := config.GetConfig()
-	// if config.IsRDBMS() {
-	// 	if err := database.InitDB().Error; err != nil {
-	// 		fmt.Println(err)
-	// 		return
-	// 	}
-	// 	// if err := migrate.StartMigration(*configure); err != nil {
-	// 	// 	fmt.Println(err)
-	// 	// 	return
-	// 	// }
-
 	err := config.Config()
 	if err != nil {
 		fmt.Println(err)
