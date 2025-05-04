6a399aac9ed40a23b3fae20215c6eb52e7e92b3f
Loknath Dhar <loknath-dhar@pm.me>
2025-05-04 17:32:11 +0600

    setup routing

--- router/router.go
+++ router/router.go
@@ -1,12 +1,17 @@
 package router
 
-// func SetUpRouter(configure config.Configuration) (*gin.Engine, error) {
-// 	if config.IsProd() {
-// 		gin.SetMode(gin.ReleaseMode)
-// 	}
-// 	router := gin.Default()
-// 	if err := router.SetTrustedProxies(nil); err != nil {
-// 		return router, err
-// 	}
-// 	return router, nil
-// }
+import (
+	"github.com/Dhar01/incident_resp/config"
+	"github.com/gin-gonic/gin"
+)
+
+func SetUpRouter(configure config.Configuration) (*gin.Engine, error) {
+	if config.IsProd() {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	router := gin.Default()
+	if err := router.SetTrustedProxies(nil); err != nil {
+		return router, err
+	}
+	return router, nil
+}
