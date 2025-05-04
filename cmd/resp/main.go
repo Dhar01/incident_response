00da332fa7e2475be48dbd67226d60afaaa33730
Loknath Dhar <loknath-dhar@pm.me>
2025-05-04 16:16:12 +0600

    will use by copying things

--- cmd/resp/main.go
+++ cmd/resp/main.go
@@ -1,49 +1,36 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Dhar01/incident_resp/router"
-	"github.com/pilinux/gorest/config"
-	"github.com/pilinux/gorest/database"
-)
-
-func main() {
-	err := config.Config()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	configure := config.GetConfig()
-	if config.IsRDBMS() {
-		if err := database.InitDB().Error; err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-
-	if config.IsRedis() {
-		if _, err := database.InitRedis(); err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-
-	if config.IsMongo() {
-		if _, err := database.InitMongo(); err != nil {
-			fmt.Println(err)
-		}
-	}
-
-	r, err := router.SetUpRouter(*configure)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	if err := r.Run(configure.Server.ServerHost + ":" + configure.Server.ServerPort); err != nil {
-		fmt.Println(err)
-		return
-	}
-}
+// func main() {
+// 	err := config.Config()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	configure := config.GetConfig()
+// 	if config.IsRDBMS() {
+// 		if err := database.InitDB().Error; err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 	}
+// 	if config.IsRedis() {
+// 		if _, err := database.InitRedis(); err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
+// 	}
+// 	if config.IsMongo() {
+// 		if _, err := database.InitMongo(); err != nil {
+// 			fmt.Println(err)
+// 		}
+// 	}
+// 	r, err := router.SetUpRouter(*configure)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	if err := r.Run(configure.Server.ServerHost + ":" + configure.Server.ServerPort); err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// }
--- router/router.go
+++ router/router.go
@@ -1,19 +1,12 @@
 package router
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/pilinux/gorest/config"
-)
-
-func SetUpRouter(configure config.Configuration) (*gin.Engine, error) {
-	if config.IsProd() {
-		gin.SetMode(gin.ReleaseMode)
-	}
-
-	router := gin.Default()
-	if err := router.SetTrustedProxies(nil); err != nil {
-		return router, err
-	}
-
-	return router, nil
-}
+// func SetUpRouter(configure config.Configuration) (*gin.Engine, error) {
+// 	if config.IsProd() {
+// 		gin.SetMode(gin.ReleaseMode)
+// 	}
+// 	router := gin.Default()
+// 	if err := router.SetTrustedProxies(nil); err != nil {
+// 		return router, err
+// 	}
+// 	return router, nil
+// }
