c07af6f5ebdeb0d170c40ded35061e956b9fad09
Loknath Dhar <loknath-dhar@pm.me>
2025-05-05 17:54:56 +0600

    add: incident model

--- internal/model/incident.go
+++ internal/model/incident.go
@@ -12,8 +12,26 @@ type Incident struct {
 	UpdatedAt time.Time      `json:"updatedAt,omitempty"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 
-	Title   string
-	Details string
-	UserID  uint
-	User    User
+	Title       string
+	Description string
+	Status      StatusType
+	Severity    SeverityType
+	AssignedTo  uint64
 }
+
+type SeverityType int
+
+const (
+	Low SeverityType = iota
+	Medium
+	High
+	Critical
+)
+
+type StatusType int
+
+const (
+	Open StatusType = iota
+	Acknowledged
+	Closed
+)
