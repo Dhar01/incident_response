5a9760b7700a6b72fa549c34bc5c909e1b084465
Loknath Dhar <loknath-dhar@pm.me>
2025-05-05 13:56:40 +0600

    update: wiring routes

--- router/router.go
+++ router/router.go
@@ -2,8 +2,11 @@ package router
 
 import (
 	"github.com/Dhar01/incident_resp/config"
+	auth_gen "github.com/Dhar01/incident_resp/router/auth"
 	"github.com/gin-gonic/gin"
 )
+
+var base string = "/api/v1"
 
 func SetUpRouter(configure config.Configuration) (*gin.Engine, error) {
 	if config.IsProd() {
@@ -12,9 +15,25 @@ func SetUpRouter(configure config.Configuration) (*gin.Engine, error) {
 
 	router := gin.Default()
 
+	// auth routes
+	authRoutes(&router.RouterGroup, base)
+
 	if err := router.SetTrustedProxies(nil); err != nil {
 		return router, err
 	}
 
 	return router, nil
+}
+
+func authRoutes(router *gin.RouterGroup, baseURL string) {
+	middlewares := []auth_gen.MiddlewareFunc{}
+
+	opt := auth_gen.GinServerOptions{
+		BaseURL:     baseURL,
+		Middlewares: middlewares,
+	}
+
+	api := newTestAPI()
+
+	auth_gen.RegisterHandlersWithOptions(router, api, opt)
 }
