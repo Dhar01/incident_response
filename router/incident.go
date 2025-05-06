69fac2e10c40270902dd1b19e803aed9adf415ef
Loknath Dhar <loknath-dhar@pm.me>
2025-05-06 17:45:40 +0600

    add: new incident creation route

--- router/incident.go
+++ router/incident.go
@@ -1,6 +1,12 @@
 package router
 
 import (
+	"net/http"
+	"reflect"
+
+	"github.com/Dhar01/incident_resp/handler"
+	"github.com/Dhar01/incident_resp/internal/model"
+	"github.com/Dhar01/incident_resp/lib/renderer"
 	incident_gen "github.com/Dhar01/incident_resp/router/incidents"
 	"github.com/gin-gonic/gin"
 )
@@ -18,7 +24,22 @@ func (api *incidentAPI) FetchIncidents(c *gin.Context) {
 }
 
 func (api *incidentAPI) CreateNewIncident(c *gin.Context) {
+	// ! user verifications will be processed on middleware
+	var req model.IncidentReq
 
+	if err := c.ShouldBindBodyWithJSON(&req); err != nil {
+		renderer.Render(c, gin.H{"message": err.Error()}, http.StatusBadRequest)
+		return
+	}
+
+	resp, statusCode := handler.CreateIncident(req)
+
+	if reflect.TypeOf(resp.Message).Kind() == reflect.String {
+		renderer.Render(c, resp, statusCode)
+		return
+	}
+
+	renderer.Render(c, resp.Message, statusCode)
 }
 
 func (api *incidentAPI) FetchIncidentByID(c *gin.Context, id uint64) {
