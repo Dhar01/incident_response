bb2fa97aac7ec946dc3e6e8a3fd1f4123a89a655
Loknath Dhar <loknath-dhar@pm.me>
2025-05-07 10:19:50 +0600

    add: all routes are protected

--- router/incident.go
+++ router/incident.go
@@ -20,6 +20,17 @@ func newIncidentAPI() *incidentAPI {
 }
 
 func (api *incidentAPI) FetchIncidents(c *gin.Context) {
+	authIDRaw, ok := c.Get("authID")
+	if !ok {
+		renderer.Render(c, gin.H{"message": "authID not found in context"}, http.StatusUnauthorized)
+		return
+	}
+
+	_, ok = authIDRaw.(uint64)
+	if !ok {
+		renderer.Render(c, gin.H{"message": "invalid authID type in context"}, http.StatusUnauthorized)
+	}
+
 	// ! need to handle cases for pagination and query params for filtering
 
 	resp, statusCode := handler.GetAllIncidents()
@@ -33,6 +44,17 @@ func (api *incidentAPI) FetchIncidents(c *gin.Context) {
 }
 
 func (api *incidentAPI) FetchIncidentByID(c *gin.Context, id uint64) {
+	authIDRaw, ok := c.Get("authID")
+	if !ok {
+		renderer.Render(c, gin.H{"message": "authID not found in context"}, http.StatusUnauthorized)
+		return
+	}
+
+	_, ok = authIDRaw.(uint64)
+	if !ok {
+		renderer.Render(c, gin.H{"message": "invalid authID type in context"}, http.StatusUnauthorized)
+	}
+
 	resp, statusCode := handler.GetIncidentByID(id)
 
 	if reflect.TypeOf(resp.Message).Kind() == reflect.String {
